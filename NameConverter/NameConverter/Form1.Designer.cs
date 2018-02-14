300a4a4358229658e69148273d1232dd9ead3f8e
Stanislav <iamlawliet@bk.ru>
2018-02-14 21:57:22 +0300

    Upload project

---
+++ NameConverter/NameConverter/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace NameConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "input path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "output path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(163, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 82);
+            this.listBox1.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(325, 230);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(43, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "test";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "008 Управление Ставропольского края по сохранению и государственной охране объект" +
+                "ов культурного наследия",
+            "017 Министерство труда и социальной защиты населения Ставропольского края"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(362, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(115, 197);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(42, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "test";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 129);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(304, 100);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ NameConverter/NameConverter/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameConverter
+{
+    public partial class Form1 : Form
+    {
+        string[] result;
+        string[] lines;
+        string pathIn;
+        string pathOut;
+        string idMinistry;
+
+        int count;
+        
+        OpenFileDialog ofd;
+        SaveFileDialog sfd;
+        Dictionary<string, string> words = new Dictionary<string, string>()
+        {
+            {"а", "a"},
+            {"б", "b"},
+            {"в", "v"},
+            {"г", "g"},
+            {"д", "d"},
+            {"е", "e"},
+            {"ё", "yo"},
+            {"ж", "zh"},
+            {"з", "z"},
+            {"и", "i"},
+            {"й", "j"},
+            {"к", "k"},
+            {"л", "l"},
+            {"м", "m"},
+            {"н", "n"},
+            {"о", "o"},
+            {"п", "p"},
+            {"р", "r"},
+            {"с", "s"},
+            {"т", "t"},
+            {"у", "u"},
+            {"ф", "f"},
+            {"х", "h"},
+            {"ц", "c"},
+            {"ч", "ch"},
+            {"ш", "sh"},
+            {"щ", "sch"},
+            {"ъ", "j"},
+            {"ы", "i"},
+            {"ь", "j"},
+            {"э", "e"},
+            {"ю", "yu"},
+            {"я", "ya"},
+            {"А", "A"},
+            {"Б", "B"},
+            {"В", "V"},
+            {"Г", "G"},
+            {"Д", "D"},
+            {"Е", "E"},
+            {"Ё", "Yo"},
+            {"Ж", "Zh"},
+            {"З", "Z"},
+            {"И", "I"},
+            {"Й", "J"},
+            {"К", "K"},
+            {"Л", "L"},
+            {"М", "M"},
+            {"Н", "N"},
+            {"О", "O"},
+            {"П", "P"},
+            {"Р", "R"},
+            {"С", "S"},
+            {"Т", "T"},
+            {"У", "U"},
+            {"Ф", "F"},
+            {"Х", "H"},
+            {"Ц", "C"},
+            {"Ч", "Ch"},
+            {"Ш", "Sh"},
+            {"Щ", "Sch"},
+            {"Ъ", "J"},
+            {"Ы", "I"},
+            {"Ь", "J"},
+            {"Э", "E"},
+            {"Ю", "Yu"},
+            {"Я", "Ya"},
+        };
+        StreamWriter sw;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ofd = new OpenFileDialog();
+            ofd.FileName = String.Empty;
+            ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            ofd.InitialDirectory = @"C:\";
+            ofd.Title = "Select txt file with names.";
+            sfd = new SaveFileDialog();
+            sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            sfd.InitialDirectory = @"C:\";
+            sfd.Title = "Select folder for txt file with names.";
+            
+            comboBox1.SelectedIndex = 1;
+        }
+
+        private string Translit(string rus)
+        {
+            string source = rus;
+            foreach (KeyValuePair<string, string> pair in words)
+                source = source.Replace(pair.Key, pair.Value);
+            return source;
+        }
+
+        private void SaveToFile(string[] input, string path)
+        {
+            sw = new StreamWriter(path);
+            for (int i = 0; i < count; i++)
+                sw.WriteLine(input[i]);
+            sw.Close();
+        }
+
+        private void ReadFromFile(string path)
+        {
+            lines = File.ReadAllLines(path, Encoding.Default);
+            count = File.ReadAllLines(path).Length;
+        }
+
+        private void Transform()
+        {
+            ReadFromFile(pathIn);
+            string tmp;
+            result = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                tmp = lines[i].ToLower();
+                tmp = Translit(tmp);
+
+                result[i] = tmp.Substring(0, tmp.IndexOf(' ')) + tmp.Substring(tmp.IndexOf(' ') + 1, 1)
+                    + tmp.Substring(tmp.LastIndexOf(' ') + 1, 1) + comboBox1.SelectedItem.ToString().Substring(0, 3);
+
+                listBox1.Items.Add(result[i]);
+            }
+
+            //foreach (string st in lines)
+            //{
+            //    tmp = st.ToLower();
+            //    tmp = Translit(tmp);
+
+            //    listBox1.Items.Add(tmp);
+            //}
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                pathIn = "input.txt";
+                pathOut = "output.txt";
+                Transform();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveToFile(result, pathOut);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pathIn = ofd.FileName;
+                label1.Text = ofd.FileName;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            idMinistry = comboBox1.SelectedItem.ToString().Substring(0, 3);
+            sfd.FileName = $"output_Names_{idMinistry}.txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pathOut = sfd.FileName;
+                    Transform();
+                    SaveToFile(result, pathOut);
+
+                    label2.Text = sfd.FileName;
+                }
+                catch (Exception ex)
+                {
+                    label3.Text = ex.ToString();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label3.Text = comboBox1.SelectedItem.ToString().Substring(0, 3);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
